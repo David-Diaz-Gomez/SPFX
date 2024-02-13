aaa4ffbe2345a0446877f60e978da223447d1387
David-Diaz-Gomez <davidgerdiaz777@gmail.com>
2024-02-13 09:57:12 -0500

    page9

---
+++ src/webparts/seleccion/components/Seleccion.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+//import styles from './Seleccion.module.scss';
+import type { ISeleccionProps } from './ISeleccionProps';
+
+export default class Seleccion extends React.Component<ISeleccionProps, {}> {
+  public render(): React.ReactElement<ISeleccionProps> {
+    const {} = this.props;
+
+    return (
+      <section >
+        
+      </section>
+    );
+  }
+}
